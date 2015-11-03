1806e68d5a57a675cb6e5bcf1fdb881670085fe6
Shae Smith <ssmith@dosomething.org>
2015-11-03 11:38:10 -0500

    Adding a editor config file for my sanity

--- database/migrations/2015_10_28_220308_drop_response_id_column.php
+++ database/migrations/2015_10_28_220308_drop_response_id_column.php
@@ -5,27 +5,27 @@ use Illuminate\Database\Migrations\Migration;
 
 class DropResponseIdColumn extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::table('messages', function (Blueprint $table) {
-            $table->dropColumn('response_id');
-        });
-    }
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::table('messages', function (Blueprint $table) {
+      $table->dropColumn('response_id');
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::table('messages', function (Blueprint $table) {
-            //
-        });
-    }
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::table('messages', function (Blueprint $table) {
+      //
+    });
+  }
 }
