8bd9f5b59053304d96625cea2a630319b65a4939
Shae Smith <ssmith@dosomething.org>
2015-11-09 14:01:07 -0500

    Style login form

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,12 +10,17 @@ use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
+  /*
+   * Show the login form.
+   */
   public function show()
   {
-    // show the form
     return View::make('users.login');
   }
 
+  /*
+   * Log the user in.
+   */
   public function login(Request $request)
   {
     $this->validate($request, [
@@ -26,50 +31,18 @@ class UserController extends Controller
     $credentials = $request->only('email', 'password');
 
     if (Auth::attempt($credentials, $request->has('remember'))) {
-      return ['result' => 'ok'];
+      return redirect('messages');
     }
 
     return redirect('login');
-
-    // // validate the info, create rules for the inputs
-    // $rules = array(
-    //   'email'    => 'required|email', // make sure the email is an actual email
-    //   'password' => 'required|alphaNum|min:3' // password can only be alphanumeric and has to be greater than 3 characters
-    // );
-
-    // // run the validation rules on the inputs from the form
-    // $validator = Validator::make($request->all(), $rules);
-
-    // // if the validator fails, redirect back to the form
-    // if ($validator->fails()) {
-    //   return redirect('login')
-    //     ->withErrors($validator) // send back all errors to the login form
-    //     ->withInput($request->except('password')); // send back the input (not the password) so that we can repopulate the form
-    // }
-    // else {
-    //   // create our user data for the authentication
-    //   $userdata = array(
-    //     'email'     => $request->input('email'),
-    //     'password'  => $request->input('password')
-    //   );
-
-    //   // attempt to do the login
-    //   if (Auth::attempt($userdata)) {
-    //     // validation successful!
-    //     // redirect them to the secure section or whatever
-    //     // return Redirect::to('secure');
-    //     // for now we'll just echo success (even though echoing in a controller is bad)
-    //     echo 'SUCCESS!';
-    //   } else {
-    //     // validation not successful, send back to form
-    //     return redirect('login');
-    //   }
-    // }
   }
 
+  /*
+   * Logout a user.
+   */
   public function logout()
   {
     Auth::logout();
-    return redirect('login'); // redirect the user to the login screen
+    return redirect('login');
   }
 }
--- resources/views/users/login.blade.php
+++ resources/views/users/login.blade.php
@@ -1,31 +1,57 @@
-<!doctype html>
+<!DOCTYPE html>
 <html>
 <head>
-  <title>Look at me Login</title>
+  <title>Uatu</title>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
+  <style>
+    .form-signin {
+      max-width: 330px;
+      padding: 15px;
+      margin: 0 auto;
+    }
+    .form-signin .form-signin-heading {
+      margin-bottom: 10px;
+    }
+    .form-signin .form-control {
+      position: relative;
+      height: auto;
+      padding: 10px;
+      font-size: 16px;
+    }
+  </style>
 </head>
 <body>
+<div class="container">
 
-{!! Form::open(array('url' => 'login')) !!}
-  <h1>Login</h1>
+  <nav class="navbar navbar-inverse">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="{{ URL::to('messages') }}">Uatu</a>
+    </div>
+  </nav>
+  <!-- will be used to show any messages -->
+  @if (Session::has('message'))
+    <div class="alert alert-info">{{ Session::get('message') }}</div>
+  @endif
 
-  <!-- if there are login errors, show them here -->
-  <p>
-      {!! $errors->first('email') !!}
-      {!! $errors->first('password') !!}
-  </p>
+  @yield('content')
 
-  <p>
-      {!! Form::label('email', 'Email Address') !!}
-      {!! Form::text('email', NULL, array('placeholder' => 'babies@uatu.com')) !!}
-  </p>
+  {!! Form::open(array('url' => 'login', 'class' => 'form-signin')) !!}
+    <h1>Login</h1>
 
-  <p>
-      {!! Form::label('password', 'Password') !!}
-      {!! Form::password('password') !!}
-  </p>
+    <div class="form-group">
+      {!! Form::label('email', 'Email Address', array('class' => 'sr-only')) !!}
+      {!! Form::text('email', NULL, array('placeholder' => 'Email', 'class' => 'form-control')) !!}
+    </div>
 
-  <p>{!! Form::submit('Log In') !!}</p>
-{!! Form::close() !!}
+    <div class="form-group">
+      {!! Form::label('password', 'Password', array('class' => 'sr-only')) !!}
+      {!! Form::password('password', array('class' => 'form-control', 'placeholder' => 'Password')) !!}
+    </div>
 
+    <p>{!! Form::submit('Log In', array('class' => 'btn btn-primary')) !!}</p>
+  {!! Form::close() !!}
+</div>
 </body>
 </html>
